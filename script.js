075c49d3fd0f6029a79aed4b9884cbeb039d40c7
Heloise Silva Santos <santoshs@taginfraestrutura.com.br>
2021-10-27 22:08:39 -0300

    Create function to animate sections with the scroll effect

--- script.js
+++ script.js
@@ -72,3 +72,30 @@ function initSmoothScroll() {
 }
 
 initSmoothScroll();
+
+function initAnimationScroll() {
+    const sections = document.querySelectorAll('.js-scroll');
+    
+    if(sections.length) {
+        const windowHalf = window.innerHeight * 0.8;
+        
+        function animaScroll() {
+            sections.forEach((section) => {
+                const sectionTop = section.getBoundingClientRect().top;
+                const isSectionVisible = sectionTop - windowHalf < 0;
+                
+                if(isSectionVisible) {
+                    section.classList.add('ativo');
+                } else {
+                    section.classList.remove('ativo');
+                }
+            });
+        }
+        
+        animaScroll();
+        
+        window.addEventListener('scroll', animaScroll);
+    }
+}
+
+initAnimationScroll();
